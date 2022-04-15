277fc4dda94be7b858d9d47371eb6264356854f5
smllygrl <hodgsonatalie@gmail.com>
2022-04-15 11:33:08 +1000

    cleared a few errors dark mode still not working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Header from "./components/Header/Header";
-import { useContext } from "react";
+import "./styling/main.scss";
 
+import { useContext } from "react";
 import { DarkThemeContext } from "./contexts/DarkThemeContext/DarkThemeContext";
+
+import Header from "./components/Header/Header";
 import ThemeToggleButton from "./components/ThemeToggleButton/ThemeToggleButton";
 
 const App = () => {
   const { darkTheme } = useContext(DarkThemeContext);
+  console.log(darkTheme);
 
   return (
     <div className={{ darkTheme } ? "theme--dark" : "theme--not-dark"}>
